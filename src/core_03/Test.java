5cbfbe6e45c6aad94fb2bb847084971982eb33ff
jucsinsun <1033784992@qq.com>
2015-12-02 10:43:28 +0800

    first commit

---
+++ src/core_03/Test.java
@@ -0,0 +1,13 @@
+package core_03;
+
+/**
+ * Created by jucsinsun on 2015/12/2.
+ */
+public class Test {
+    public static void main (String arg[]){
+
+        System.out.println("hello World");
+
+
+    }
+}
